4c43df6191a5ed60f84e1f084550db456f816f83
gjakobson <59399834+gjakobson@users.noreply.github.com>
2025-05-29 12:20:47 -0700

    working baseline of analytics example
    ready for submission

---
+++ samples/python/agents/analytics/__main__.py
@@ -0,0 +1,94 @@
+"""This file serves as the main entry point for the application.
+
+It initializes the A2A server, defines the agent's capabilities,
+and starts the server to handle incoming requests. Notice the agent runs on port 10011.
+"""
+
+import logging
+import os
+
+import click
+
+from a2a.server.apps import A2AStarletteApplication
+from a2a.server.request_handlers import DefaultRequestHandler
+from a2a.server.tasks import InMemoryTaskStore
+from a2a.types import (
+    AgentCapabilities,
+    AgentCard,
+    AgentSkill,
+)
+from agent import ChartGenerationAgent
+from agent_executor import ChartGenerationAgentExecutor
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+class MissingAPIKeyError(Exception):
+    """Exception for missing API key."""
+
+    pass
+
+
+@click.command()
+@click.option('--host', 'host', default='localhost')
+@click.option('--port', 'port', default=10011)
+def main(host, port):
+    """Entry point for the A2A + CrewAI Image generation sample."""
+    try:
+        if not os.getenv('GOOGLE_API_KEY') and not os.getenv(
+            'GOOGLE_GENAI_USE_VERTEXAI'
+        ):
+            raise MissingAPIKeyError(
+                'GOOGLE_API_KEY or Vertex AI environment variables not set.'
+            )
+
+        capabilities = AgentCapabilities(streaming=False)
+        skill = AgentSkill(
+            id='image_generator',
+            name='Image Generator',
+            description=(
+                'Generate a chart based on CSV-like data passed in'
+            ),
+            tags=['generate image', 'edit image'],
+            examples=['Generate a photorealistic image of raspberry lemonade'],
+        )
+
+        agent_card = AgentCard(
+            name='Chart Generator Agent',
+            description=(
+                'Generate charts from structured CSV-like data input.'
+            ),
+            url=f'http://{host}:{port}/',
+            version='1.0.0',
+            defaultInputModes=ChartGenerationAgent.SUPPORTED_CONTENT_TYPES,
+            defaultOutputModes=ChartGenerationAgent.SUPPORTED_CONTENT_TYPES,
+            capabilities=capabilities,
+            skills=[skill],
+        )
+
+        request_handler = DefaultRequestHandler(
+            agent_executor=ChartGenerationAgentExecutor(),
+            task_store=InMemoryTaskStore(),
+        )
+        server = A2AStarletteApplication(
+            agent_card=agent_card, http_handler=request_handler
+        )
+        import uvicorn
+
+        uvicorn.run(server.build(), host=host, port=port)
+
+    except MissingAPIKeyError as e:
+        logger.error(f'Error: {e}')
+        exit(1)
+    except Exception as e:
+        logger.error(f'An error occurred during server startup: {e}')
+        exit(1)
+
+
+if __name__ == '__main__':
+    main()
