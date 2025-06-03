291eca11c0fb2c8ca0bfd454a223633351791ba2
Dai-H15 <s1f102200828@iniad.org>
2025-06-03 10:38:19 +0900

    Automatically prepend http:// to agent address if missing

--- demo/ui/utils/agent_card.py
+++ demo/ui/utils/agent_card.py
@@ -6,7 +6,7 @@ from a2a.types import AgentCard
 def get_agent_card(remote_agent_address: str) -> AgentCard:
     """Get the agent card."""
     if not remote_agent_address.startswith(('http://', 'https://')):
-        raise ValueError("remote_agent_address must start with http:// or https://")
+        remote_agent_address = 'http://' + remote_agent_address
     agent_card = requests.get(
         f'{remote_agent_address}/.well-known/agent.json'
     )
